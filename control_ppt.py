f6b5e329069195af31560618abe5c096275fdbc5
Byllie <90178612+Byllie@users.noreply.github.com>
2022-01-21 11:16:43 +0100

    Update control_ppt.py
    Creation de control_ppt

--- control_ppt.py
+++ control_ppt.py
@@ -1,1 +1,18 @@
-
+import pyautogui
+import time
+mode_de_fonctionnemnt=0
+temps=0
+def mouvement(mouv):
+    global temps
+    global mode_de_fonctionnemnt
+    if mode_de_fonctionnemnt==0:
+        if time.time()-temps>5:
+            if mouv=="Bras-Droit":
+                pyautogui.press('left')
+            elif mouv=="Bras-Gauche":
+                pyautogui.press('right')
+            if type(mouv)==int:
+                mode_de_fonctionnemnt=mouv
+            temps=time.time()
+    else:
+        pass
