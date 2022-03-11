058666414ea10cb5fd20b96562a2811720e87a94
Byllie <90178612+Byllie@users.noreply.github.com>
2022-03-11 10:31:31 +0100

    Update vecteur.py
    Ajout d'une classe déplacement qui permet de caractériser un mouvement

--- vecteur.py
+++ vecteur.py
@@ -7,4 +7,9 @@ class Vecteur:
         self.module=math.sqrt(self.affixe.real**2+self.affixe.imag**2)
         self.argument=math.acos(self.affixe.real/self.module) if (math.acos(self.affixe.real/self.module) *math.asin(self.affixe.imag/self.module)>=0) else math.acos(self.affixe.real/-self.module)+math.pi
         #vraiment impresionnant je suis vraiment le roi du sinus (Gwendal)
-
+class Deplacement:
+    def __init__(self,pourcentage,angleGrandVecteur,anglePetitVecteur,fonctionmouvement):
+        self.p=pourcentage
+        self.a1=angleGrandVecteur
+        self.a2=anglePetitVecteur
+        self.f=fonctionmouvement
