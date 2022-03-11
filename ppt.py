08d91c51f495ed82766085c82c3a034aa716dd8f
Byllie <90178612+Byllie@users.noreply.github.com>
2022-03-11 11:31:35 +0100

    Modif pour rendre plus simple

--- ppt.py
+++ ppt.py
@@ -1,18 +1,19 @@
 import pyautogui
 import time
-mode_de_fonctionnemnt=0
 temps=0
-def mouvement(mouv):
+
+def droite():
     global temps
-    global mode_de_fonctionnemnt
-    if mode_de_fonctionnemnt==0:
-        if time.time()-temps>1:
-            if mouv=="Bras-Droit":
-                pyautogui.press('left')
-            elif mouv=="Bras-Gauche":
-                pyautogui.press('right')
-            if type(mouv)==int:
-                mode_de_fonctionnemnt=mouv
-            temps=time.time()
+    if time.time()-temps>1:
+        pyautogui.press('right')
+        temps=time.time()
+    else:
+        pass
+
+def gauche():
+    global temps
+    if time.time()-temps>1:
+        pyautogui.press('left')
+        temps=time.time()
     else:
         pass
