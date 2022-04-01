e94148d9c33cadc65eeec3378f1dc648716fe542
Joseph Jaujay <87907417+josephjaujay@users.noreply.github.com>
2022-04-01 11:41:54 +0200

    Update test-detection-couleur.py

--- test-detection-couleur.py
+++ test-detection-couleur.py
@@ -1,12 +1,14 @@
 import cv2
 import math
-import numpy as np
+from numpy import *
 
 def test () :
     video = cv2.VideoCapture(0)
+
     # Couleurs à détecter (bleu).
-    d = np.array([95, 100, 50])
-    f = np.array([105, 255, 255])
+    d = array([95, 100, 50])
+    f = array([105, 255, 255])
+
     while True :
         ret, frame = video.read()
 
@@ -17,19 +19,18 @@ def test () :
         x, y, largeur, longueur = cv2.boundingRect(mask)
 
         coord = (x + (largeur//2), y + (longueur//2))
-
-        print(coord)
+        cv2.circle(frame, coord, 3, (0, 0, 255), -1)
         cv2.imshow("1", mask)
         cv2.imshow("2", frame)
         #cv2.imshow("3", j)
-        #cv2.imsqhow("4", i)
+        #cv2.imshow("4", i)
 
 
 
 
         if cv2.waitKey(1) & 0xFF == ord('q') :
             break
-    print(frame)
+
     video.release()
     cv2.destroyAllWindows()
 
