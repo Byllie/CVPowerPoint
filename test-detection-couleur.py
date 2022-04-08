78a7a21db608af3524461b0bf9eb02f083812187
Joseph Jaujay <87907417+josephjaujay@users.noreply.github.com>
2022-04-08 11:29:55 +0200

    Update test-detection-couleur.py

--- test-detection-couleur.py
+++ test-detection-couleur.py
@@ -2,28 +2,63 @@ import cv2
 import math
 from numpy import *
 
+from time import *
+
 def test () :
     video = cv2.VideoCapture(0)
 
+
+
+
+    taille = (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+    tm = time()
+
+    while time()-tm <= 5:
+        ret, frame = video.read()
+
+        coord=(taille[0]//2, taille[1]//2)
+        cv2.circle(frame, coord, 3, (255, 0, 0), -1)
+        cv2.imshow("bla", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q') :
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+    c=frame[coord[0]][coord[1]]
+    print(c)
+    video = cv2.VideoCapture(0)
+
     # Couleurs à détecter (bleu).
-    d = array([95, 100, 50])
-    f = array([105, 255, 255])
+    d = array([int(c[0]*0.7), int(c[1]*0.7), int(c[2]*0.7)])
+    f = array([int(c[0]*1.3), int(c[1]*1.3), int(c[2]*1.3)])
+    print(d, f)
 
     while True :
         ret, frame = video.read()
+
 
         i = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(i, d, f)
         j = cv2.bitwise_and(frame, frame, mask=mask)
 
         x, y, largeur, longueur = cv2.boundingRect(mask)
+        r, g, b = cv2.split(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV))
+
 
         coord = (x + (largeur//2), y + (longueur//2))
         cv2.circle(frame, coord, 3, (0, 0, 255), -1)
         cv2.imshow("1", mask)
         cv2.imshow("2", frame)
         #cv2.imshow("3", j)
-        #cv2.imshow("4", i)
+
+
+        print(frame[100][100])
+        #cv2.imshow("5", r)
+        #cv2.imshow("6", g)
+        #cv2.imshow("7", b)
+        cv2.imshow("4", i)
 
 
 
