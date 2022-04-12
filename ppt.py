3329a9703903db3b2a8053464a6455c719fa12d9
Byllie <90178612+Byllie@users.noreply.github.com>
2022-04-12 14:01:10 +0200

    Ajout des nouvelles commandes pour le mode musique

--- ppt.py
+++ ppt.py
@@ -17,3 +17,36 @@ def gauche():
         temps=time.time()
     else:
         pass
+
+
+def volumeplus():
+    global temps
+    if time.time()-temps>1:
+        pyautogui.press('volumeup')
+        temps=time.time()
+    else:
+        pass
+
+def volumemoins():
+    global temps
+    if time.time()-temps>1:
+        pyautogui.press('volumedown')
+        temps=time.time()
+    else:
+        pass
+
+def suivant():
+    global temps
+    if time.time()-temps>1:
+        pyautogui.press('nexttrack')
+        temps=time.time()
+    else:
+        pass
+
+def precedent():
+    global temps
+    if time.time()-temps>1:
+        pyautogui.press('prevtrack')
+        temps=time.time()
+    else:
+        pass
